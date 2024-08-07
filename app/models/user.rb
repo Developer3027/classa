b988120cd836322e4c4665556f39a89d371c059a
Mason Roberts <mrobe18@gmail.com>
2024-08-06 21:37:18 -0400

    devise user set up. Roles set up. Base fields added to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,21 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # The `role` column is an enum that can only take on the values `:user` and `:admin`.
+  enum role: [:user, :admin]
+  # The `set_default_role` method is called automatically by the `after_initialize` callback
+  after_initialize :set_default_role, if: :new_record?
+
+  # Sets the default role for a user if it has not been set.
+  #
+  # This method is called automatically by the `after_initialize` callback
+  # when a new user record is created. It assigns the `:user` role to the
+  # `role` attribute if it is currently `nil`.
+  #
+  # @return [void]
+  def set_default_role
+    self.role ||= :user
+  end
+
 end
